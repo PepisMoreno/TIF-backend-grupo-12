8418fc1704142e63c510b8c56a6d7f322f398b20
Spizzamus <sofaina1965@gmail.com>
2023-11-27 19:05:10 -0300

    Agrego mysql a la app..

--- api-de-prueba/app.py
+++ api-de-prueba/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request 
+#No me toma flaskext.mysql
+# from flasketc.mysql import MySQL
+
 #request sirve para recolectar la data que viene del formulario
 app = Flask(__name__)
+mysql = Flask(__name__)
+
 
 @app.route("/",methods=["GET"])
 def home():
