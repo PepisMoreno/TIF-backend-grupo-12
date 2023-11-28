1cbc22ea5c9b268291ffd3164f3075afc87c8195
Spizzamus <sofaina1965@gmail.com>
2023-11-27 21:42:18 -0300

    último commit del día. Agregué instalación de Flask-MySQL en requirements y cambios en app

--- api-de-prueba/app.py
+++ api-de-prueba/app.py
@@ -1,7 +1,7 @@
 #Ult 27-11 Sofi
 from flask import Flask, render_template, request 
 #Sofi: No me toma flaskext.mysql, puse sólo flask
-from flask import MySQL
+from flaskext.mysql import MySQL
 
 #Sofi: request sirve para recolectar la data que viene del formulario
 app = Flask(__name__)
@@ -18,25 +18,25 @@ mysql.init_app(app)
 
 #Decorador
 @app.route("/",methods=["GET"])
-
-#Sofi: para probar mysql 
-#def index():
-#    conn = mysql.connect()
-#    cursor = conn.cursor()
-
-#    sql="INSERT INTO `características` VALUES (1,'sin luz directa','mucha','suelo/roca/troncos','superficies porosas','-')"
-#    cursor.execute(sql)
-
-#    conn.commit()
-
-#    return render_template('templates/index.html')
-
 def home():
     return render_template("index.html")
+#Sofi: para probar mysql 
+def index():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+#    sql="INSERT INTO `características` VALUES (1,'sin luz directa','mucha','suelo/roca/troncos','superficies porosas','-')"
+    cursor.execute(mysql)
+
+    conn.commit()
+
+    return render_template('templates/index.html')
+
+
 
 
 #Recordar: para correr app es python source/app.py
-#if __name__ == '__main__':
-#    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
     
 
