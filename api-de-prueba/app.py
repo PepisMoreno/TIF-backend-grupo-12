3dc889bc39b2c2642fa1a31d9bd332d03266b55a
Pepis Moreno <143961133+PepisMoreno@users.noreply.github.com>
2023-12-06 16:03:24 -0300

    Delete api-de-prueba directory

--- api-de-prueba/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request 
-#request sirve para recolectar la data que viene del formulario
-app = Flask(__name__)
-
-@app.route("/",methods=["GET"])
-def home():
-    return render_template("index.html")
-
-
